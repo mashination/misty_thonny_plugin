83cf19f5c0f3f43f102b26211d2ee807d30a3fab
mashination <63434209+mashination@users.noreply.github.com>
2022-03-29 13:50:29 +0200

    Update mistyPlugin.py

--- thonnycontrib/mistyPlugin.py
+++ thonnycontrib/mistyPlugin.py
@@ -3,12 +3,17 @@ from tkinter.messagebox import showinfo
 
 from thonny import get_shell
 from thonny.editors import get_saved_current_script_filename
+from thonny.editors import extract_target_path, is_local_path
 
 PATH_MISTY = "misty/Misty.py "
 
 def run():
     filename = get_saved_current_script_filename()
-    get_shell().submit_magic_command("%Run " + PATH_MISTY +  "\"" + filename + "\"")
+    if  is_local_path(filename):
+        target_path = filename
+    else:
+        target_path = extract_target_path(filename)
+    get_shell().submit_magic_command("%Run " + PATH_MISTY +  "\"" + target_path + "\"")
 
 def load_plugin():
     get_workbench().add_command(command_id="misty",
